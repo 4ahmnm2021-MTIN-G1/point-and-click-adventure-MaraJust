5287fffffa3b69cf14f7557cd19fed9e7c4e87cc
MaraJust <mara.just03@htl-salzburg.ac.at>
2020-11-09 12:32:44 +0100

    add assets and scripts

---
+++ Assets/Scripts/Hunter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : MonoBehaviour
+{
+   // Klasse   -> Bauplan
+   //Klasse    -> Jäger
+
+   // Variablen sind ein Container -> Werte oder Referenzen
+   // Variable die Werte speichern
+
+   //public             -> access modifier
+   // string            -> Type of variable
+   // nameOfCharacter   -> identifier (name of variable)
+   // ;                 -> end of Statement
+   public string nameOfCharacter; // Deklaration einer Variable   (Instanzvariable)
+   public int ageOfCharacter;    //                               (Instanzvariable)
+   public float sizeOfCharacter; //                               (Instanzvariable)
+   public bool isWearingGlasses;
+
+   // Referenztyp Variablen (Verweis)
+   public GameObject newGameObject;
+   public BoxCollider bc;
+   public SpriteRenderer sr;
+
+   public Hunter buddy;
+
+   //public    -> access modifier
+   // void     -> return type (wird ein wert zurückgegeben)
+   // Start    -> Name der Funktion
+   // ()       -> Parameterliste (in diesem Fall leer)
+   // {}       -> Scope / Wirkungsbereich (Was alles Teil der Funktion ist)
+  
+   // Deklaration einer Funktion
+   public void Start ()
+   {
+      buddy.isWearingGlasses = false;
+      // Hier wird etwas ausgeführt
+   }
+
+   public void ZweiteFunktion () 
+   {
+      // Hier wird etwas ausgeführt
+   }
+
+
+   // Objekte -> Instanze einer Klasse
+   // Objekt -> MEIN Jäger
+}
---
+++ Assets/Scripts/ItemActions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemActions : MonoBehaviour
+{
+    public GameObject actionPanel;
+    public PolygonCollider2D collider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start aufgerufen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+       Debug.Log("onmousedown aufgerufen");
+       actionAnimation();
+    }
+
+    public void actionAnimation () {
+
+        actionPanel.SetActive(true);
+    }
+
+}
